53d3f511e915cf56ebc29b93bc304d69d92bc814
Diederik van der Hoek <Died@Diederiks-MacBook-Air.local>
2016-04-14 14:17:33 +0800

    added methods required by assignment

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,2 +1,15 @@
 class Legislator < ActiveRecord::Base
+
+	:title
+	:firstname
+	:middlename
+	:lastname
+	:name_suffix
+	
+	def self.state(name_state)
+		list = self.where("state = ? AND title = ?", name_state, "Rep")
+		list.each {|rep| puts "#{rep.name_suffix} #{rep.firstname} #{rep.lastname}"}
+		end
+	end
+		
 end
